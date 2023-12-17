b71ba6559330c6ef976c3bcb683c0f04015c3adb
balssh <george.bals25@gmail.com>
2023-12-17 23:35:32 +0200

    add a bit of changes to weather

--- ags/js/bar/buttons/Weather.js
+++ ags/js/bar/buttons/Weather.js
@@ -1,6 +1,13 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import WeatherService from "../../services/weather.js";
 import PanelButton from "../PanelButton.js";
+import HoverRevealer from "../../misc/HoverRevealer.js";
+import PopupWindow from "../../misc/PopupWindow.js";
+
+/**
+ * @param {Object} o
+ * @param {import('../../misc/HoverRevealer').HoverRevealProps['direction']=} o.direction
+ */
 
 const WeatherBox = () =>
   Widget.Box({
@@ -19,12 +26,32 @@ const WeatherBox = () =>
     binds: [["tooltip-markup", WeatherService, "description"]],
   });
 
-const WeatherButton = () =>
-  PanelButton({
-    class_name: "weather",
-    window: "weather",
-    // on_clicked: () => App.toggleWindow("weather"),
-    content: WeatherBox(),
+// TODO: Add a popup window with more info
+const WeatherAdditionalInfo = () =>
+  Widget.Label({
+    class_name: "weather-additional-info",
+    binds: [
+      [
+        "label",
+        WeatherService,
+        "weather_data",
+        (out) => {
+          // const { current_condition, nearest_area, request } = out;
+          // const { temp_C, FeelsLikeC, weatherDesc } = request[0];
+          const { value: city } = out.nearest_area[0].areaName[0];
+          // const { query } = request[0];
+          return `${city}`;
+        },
+      ],
+    ],
   });
 
-export default WeatherButton;
+const WeatherRevealer = ({ direction = "left" } = {}) =>
+  HoverRevealer({
+    class_name: "weather panel-button",
+    direction,
+    indicator: WeatherBox(),
+    child: WeatherAdditionalInfo(),
+  });
+
+export default WeatherRevealer;
